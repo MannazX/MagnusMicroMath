3cb2682b1edafdf12eb53d45eabdb405365c6a4c
Magnus Sverdrup <magger254@gmail.com>
2025-04-10 10:41:25 +0200

    Comments for the Add method

--- MagnusMicroMath/MagnusMicroMath.cs
+++ MagnusMicroMath/MagnusMicroMath.cs
@@ -8,6 +8,12 @@ namespace MagnusMicroMath
 {
     public class MagnusMicroMath
     {
+        /// <summary>
+        /// Method for adding two integers together
+        /// </summary>
+        /// <param name="t1">First integer</param>
+        /// <param name="t2">Second integer</param>
+        /// <returns>An integer sum of the two parameter integers</returns>
         public int Add(int t1, int t2)
         {
             return t1 + t2;
